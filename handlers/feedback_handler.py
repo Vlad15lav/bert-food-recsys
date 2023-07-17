eda728906ea5d69dd4c86753b957e2b352e6471c
Vladislav Viryasov <62297940+Vlad15lav@users.noreply.github.com>
2023-07-17 19:17:32 +0300

    update handlers py scripts

--- handlers/feedback_handler.py
+++ handlers/feedback_handler.py
@@ -5,21 +5,15 @@ def feedback_handler(bot, message, db):
     empty_markup = ReplyKeyboardRemove()
 
     if message.text == "Впечатляет😋":
-        add_reviews(db, message.from_user.username, 5)
+        add_reviews(db, message.from_user.id, 3)
         bot.send_message(message.chat.id, text="Спасибо за отзыв!", parse_mode='Markdown', reply_markup=empty_markup)
-    elif message.text == "Интересно🙂":
-        add_reviews(db, message.from_user.username, 4)
-        bot.send_message(message.chat.id, text="Спасибо за отзыв!", parse_mode='Markdown', reply_markup=empty_markup)
-    elif message.text == "Стандартно😐":
-        add_reviews(db, message.from_user.username, 3)
-        bot.send_message(message.chat.id, text="Спасибо за отзыв!", parse_mode='Markdown', reply_markup=empty_markup)
-    elif message.text == "Не уверен😕":
-        add_reviews(db, message.from_user.username, 2)
+    elif message.text == "Нормально😐":
+        add_reviews(db, message.from_user.id, 2)
         bot.send_message(message.chat.id, text="Спасибо за отзыв!", parse_mode='Markdown', reply_markup=empty_markup)
     elif message.text == "Не впечатляет😔":
-        add_reviews(db, message.from_user.username, 1)
+        add_reviews(db, message.from_user.id, 1)
         bot.send_message(message.chat.id, text="Спасибо за отзыв!", parse_mode='Markdown', reply_markup=empty_markup)
     else:
         return False
-    
+
     return True
--- handlers/recommendation_handler.py
+++ handlers/recommendation_handler.py
@@ -5,7 +5,7 @@ from telebot import types
 from utils.loader import load_data, load_model_tfidf, load_model_bert
 from models.recommendation_model import get_bert_recommendation, get_tfidf_recomendation
 
-from database.queries import get_id_model, get_id_user, add_request
+from database.queries import get_model_id, get_user_id, add_request
 
 data_items, bert_embed_items, tfidf_embed_items = load_data()
 
@@ -21,37 +21,39 @@ def recommendation_handler(bot, message, db):
     """
     Создаем рекомендацию для пользователя
     """
-    id_model = get_id_model(db, message.from_user.username)
+    model_id = get_model_id(db, message.from_user.id)
 
-    if id_model is None:
-        id_model = int(random.random() < 0.5) + 1
+    # Выбираем модель рекомендации для пользователя
+    if model_id is None:
+        model_id = int(random.random() < 0.5) + 1
 
     # Получаем рейтинг сходства рецептов
-    if MODELS_NUM[id_model] == "TF-IDF":
+    if MODELS_NUM[model_id] == "TF-IDF":
         ranking = get_tfidf_recomendation(message.text, tfidf_model, tfidf_embed_items)
     else:
         ranking = get_bert_recommendation(message.text, tokenizer, bert_model, bert_embed_items)
-    
+
     # Создаем текст рекомендации
-    id_item = int(ranking[0])
-    text_send = "Название: {}\nСостав: {}\n{}. Время готовки: {}".format(*data_items.loc[[id_item],
-            ['name', 'ingredient', 'type_kitchen', 'time_cook']].values.reshape(-1))
-    text_send += f"\n\nПодробнее рецепт по [ссылке]({data_items.loc[id_item, 'link']})"
+    items_id = [int(ranking[i]) for i in range(3)]
+
+    # Сохраняем рекомендацию в базу данных
+    user_id = get_user_id(db, message.from_user.id)
+    add_request(db, user_id, items_id, model_id)
 
     # Создаем UI кнопки бота для оценки рекомендации
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn_eval_5 = types.KeyboardButton("Впечатляет😋")
-    btn_eval_4 = types.KeyboardButton("Интересно🙂")
-    btn_eval_3 = types.KeyboardButton("Стандартно😐")
-    btn_eval_2 = types.KeyboardButton("Не уверен😕")
+    btn_eval_3 = types.KeyboardButton("Впечатляет😋")
+    btn_eval_2 = types.KeyboardButton("Нормально😐")
     btn_eval_1 = types.KeyboardButton("Не впечатляет😔")
-    markup.add(btn_eval_5, btn_eval_4, btn_eval_3, btn_eval_2, btn_eval_1)
+    markup.add(btn_eval_3, btn_eval_2, btn_eval_1)
 
-    # Отправляем рекомендацию
-    bot.send_message(message.chat.id, text=text_send, parse_mode='Markdown', reply_markup=markup)
+    for item_id in items_id:
+        text_send = "Название: {}\nСостав: {}\n{}. Время готовки: {}".format(*data_items.loc[[item_id],
+            ['name', 'ingredient', 'type_kitchen', 'time_cook']].values.reshape(-1))
+        text_send += f"\n\nПодробнее рецепт по [ссылке]({data_items.loc[item_id, 'link']})"
+
+        # Отправляем рекомендацию
+        bot.send_message(message.chat.id, text=text_send, parse_mode='Markdown', reply_markup=markup)
+    
     if random.random() < 0.5:
         bot.send_message(message.chat.id, text="Оцените пожалуйста рекомендацию", parse_mode='Markdown', reply_markup=markup)
-
-    # Сохраняем рекомендацию в базу данных
-    id_user = get_id_user(db, message.from_user.username)
-    add_request(db, id_user, id_item, id_model)
