378d26cc3f346b14e3e912c7b2f58a31cc7210f0
Vladislav Viryasov <62297940+Vlad15lav@users.noreply.github.com>
2023-07-17 19:14:10 +0300

    update recommendation_model.py

--- models/recommendation_model.py
+++ models/recommendation_model.py
@@ -17,12 +17,15 @@ def get_bert_recommendation(text, tokenizer, model, embed_items):
     """
     Вычисления схожих продуктов для рекомендации с помощью BERT
     """
+    # Преобразования текста в токены
     text_tokens = tokenizer(text, **CONFIG['TOKENIZER'])
 
+    # Векторное представление BERT модели
     with torch.no_grad():
         out = model(**text_tokens)
         out = out.last_hidden_state[:, 0, :].cpu().numpy()
 
+    # Получаем схожесть рецептов и сортируем по значению
     rating = cosine_similarity(embed_items, out).reshape(-1)
     rating_arg = np.argsort(rating)[::-1]
     
@@ -33,16 +36,21 @@ def get_tfidf_recomendation(text, model, embed_items):
     """
     Вычисления схожих продуктов для рекомендации с помощью TF-IDF
     """
+    # Фильтрация текста
     text = text.lower()
     text = re.sub(r'\([^()]*\)', '', text)
     text = re.sub(r'[^а-яё]', ' ', text)
     text = re.sub(" +", " ", text)
 
+    # Удаляем стоп-слова и нормализуем
     tokens = mystem.lemmatize(text)
     text = " ".join([token for token in tokens\
                     if not token in stopwords_ru and token != " " and len(token) > 1])
+    
+    # Векторное представление TF-IDF модели
     out = model.transform([text])
 
+    # Получаем схожесть рецептов и сортируем по значению
     rating = cosine_similarity(embed_items, out).reshape(-1)
     rating_arg = np.argsort(rating)[::-1]
     
