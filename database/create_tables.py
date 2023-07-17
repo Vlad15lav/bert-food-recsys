f79f4711f92761b04b6c7702554aaabd9b7dedb5
Vladislav Viryasov <62297940+Vlad15lav@users.noreply.github.com>
2023-07-17 14:26:11 +0300

    update database py files

--- database/create_tables.py
+++ database/create_tables.py
@@ -9,31 +9,33 @@ def create_tables(db):
     try:
         with db.cursor() as cur:
             cur.execute("""CREATE TABLE IF NOT EXISTS users (
-                            id_user INT NOT NULL AUTO_INCREMENT,
-                            user_name CHAR(50) NOT NULL,
-                            PRIMARY KEY (id_user));""")
+                            user_id INT NOT NULL AUTO_INCREMENT,
+                            tm_hash_id CHAR(64) NOT NULL,
+                            PRIMARY KEY (user_id))""")
 
             cur.execute("""CREATE TABLE IF NOT EXISTS models (
-                            id_model INT NOT NULL AUTO_INCREMENT,
-                            system_name CHAR(12) NOT NULL,
-                            PRIMARY KEY(id_model));""")
+                            model_id INT NOT NULL AUTO_INCREMENT,
+                            system_name VARCHAR(16) NOT NULL,
+                            PRIMARY KEY(model_id))""")
 
             cur.execute("""CREATE TABLE IF NOT EXISTS requests (
-                            id_req INT NOT NULL AUTO_INCREMENT,
-                            id_user INT NOT NULL,
-                            id_item INT NOT NULL,
-                            id_model INT NOT NULL,
+                            request_id INT NOT NULL AUTO_INCREMENT,
+                            user_id INT NOT NULL,
+                            item_first_id INT NOT NULL,
+                            item_second_id INT NOT NULL,
+                            item_third_id INT NOT NULL,
+                            model_id INT NOT NULL,
                             date DATETIME NOT NULL,
-                            PRIMARY KEY(id_req));""")
+                            PRIMARY KEY(request_id))""")
 
             cur.execute("""CREATE TABLE IF NOT EXISTS reviews (
-                            id_rev INT NOT NULL AUTO_INCREMENT,
-                            id_req INT NOT NULL,
+                            review_id INT NOT NULL AUTO_INCREMENT,
+                            request_id INT NOT NULL,
                             rating INT NOT NULL,
-                            PRIMARY KEY(id_rev));""")
+                            PRIMARY KEY(review_id))""")
 
-            cur.execute("""INSERT IGNORE INTO models (id_model, system_name)
-                            VALUES (1, 'TF-IDF'), (2, 'BERT');""")
+            cur.execute("""INSERT IGNORE INTO models (model_id, system_name)
+                            VALUES (1, 'TF-IDF'), (2, 'BERT')""")
     except Exception as e:
         print(e)
     
--- database/queries.py
+++ database/queries.py
@@ -1,92 +1,116 @@
 import mysql.connector
+import hashlib
 
-def add_user(db, user_name):
+def add_user(db, user_tm_id):
     """
     Добавляем нового пользователя в базу данных
     """
-    with db.cursor() as cur:
-        cur.execute("SELECT id_user \
-                        FROM users \
-                        WHERE user_name = %s", (user_name, ))
-        result_query = cur.fetchall()
+    tm_hash_id = hashlib.sha3_256(str(user_tm_id).encode()).hexdigest()
 
-        if len(result_query) == 0:
-            cur.execute("INSERT INTO users (user_name) \
-                            VALUES (%s)", (user_name, ))
+    with db.cursor() as cur:
+        cur.execute("""SELECT user_id
+                        FROM users
+                        WHERE tm_hash_id = %s""", (tm_hash_id, ))
+        user_id = cur.fetchall()
+
+        # Если пользователя еще нет базе данных - добавляем его
+        if len(user_id) == 0:
+            cur.execute("""INSERT INTO users (tm_hash_id)
+                            VALUES (%s)""", (tm_hash_id, ))
     db.commit()
 
 
-def get_id_user(db, user_name):
+def get_user_id(db, user_tm_id):
     """
     Узнать индекс пользователя в базе данных
     """
+    tm_hash_id = hashlib.sha3_256(str(user_tm_id).encode()).hexdigest()
+
     with db.cursor() as cur:
-        cur.execute("SELECT id_user \
-                        FROM users \
-                        WHERE user_name = %s", (user_name, ))
-        user_id = cur.fetchall()[0][0]
-    
-    return user_id
+        cur.execute("""SELECT user_id
+                        FROM users
+                        WHERE tm_hash_id = %s""", (tm_hash_id, ))
+        user_id = cur.fetchall()
+
+    # Если пользователя еще нет базе данных - добавляем его
+    if len(user_id) == 0:
+        with db.cursor() as cur:
+            cur.execute("""INSERT INTO users (tm_hash_id)
+                            VALUES (%s)""", (tm_hash_id, ))
+            db.commit()
+
+            cur.execute("""SELECT user_id
+                            FROM users
+                            WHERE tm_hash_id = %s""", (tm_hash_id, ))
+            user_id = cur.fetchall()
+
+    return user_id[0][0]
 
 
-def add_request(db, id_user, id_item, id_model):
+def add_request(db, user_id, items_id, model_id):
     """
     Добавляем запрос рекомендации в базу данных
     """
     with db.cursor() as cur:
-        cur.execute("INSERT INTO requests (id_user, id_item, id_model, date) \
-                        VALUES (%s, %s, %s, CONVERT_TZ(NOW(),'+00:00','+3:00'))",
-                        (id_user, id_item, id_model))
-    
+        cur.execute("""INSERT INTO requests (user_id, item_first_id,
+                                item_second_id, item_third_id, model_id, date)
+                        VALUES (%s, %s, %s, %s, %s, CONVERT_TZ(NOW(),'+00:00','+3:00'))""",
+                        (user_id, *items_id, model_id))
+
     db.commit()
 
 
-def get_id_model(db, user_name):
+def get_model_id(db, user_tm_id):
     """
-    Узнать текущею рекомендательную систему у пользователя
+    Узнать текущею рекомендательную систему у пользователя (для A/B тестирования)
     """
-    with db.cursor() as cur:
-        cur.execute(f"SELECT r.id_model \
-                        FROM requests AS r \
-                        JOIN users AS u USING(id_user) \
-                        WHERE u.user_name = '{user_name}' \
-                            AND DATE(r.date) = (SELECT DATE(CONVERT_TZ(NOW(),'+00:00','+3:00')));")
-        id_model = cur.fetchall()
-    
-    if len(id_model) == 0:
-        return None
-    
-    return id_model[0][0]
+    tm_hash_id = hashlib.sha3_256(str(user_tm_id).encode()).hexdigest()
 
-def add_reviews(db, user_name, rating):
+    with db.cursor() as cur:
+        cur.execute("""SELECT r.model_id
+                        FROM requests AS r
+                        JOIN users AS u USING(user_id)
+                        WHERE u.tm_hash_id = %s
+                            AND DATE(r.date) = (SELECT DATE(CONVERT_TZ(NOW(),'+00:00','+3:00')))""",
+                            (tm_hash_id, ))
+        model_id = cur.fetchall()
+
+    if len(model_id) == 0:
+        return None
+
+    return model_id[0][0]
+
+
+def add_reviews(db, user_tm_id, rating):
     """
     Добавляем отзыв пользователя о рекомендации (для A/B тестирования)
     """
     # Проверяем есть ли рекомендация, которую нужно оценить
-    with db.cursor() as cur:
-        cur.execute(f"SELECT MAX(r.id_req) \
-                        FROM requests AS r \
-                        JOIN users AS u USING(id_user) \
-                        WHERE u.user_name = '{user_name}';")
-        id_req = cur.fetchall()
+    tm_hash_id = hashlib.sha3_256(str(user_tm_id).encode()).hexdigest()
 
-    if len(id_req) == 0:
-        return
-    
-    # Проверяем оставлял ли уже пользователь отзыв на рекомендацию
     with db.cursor() as cur:
-        cur.execute(f"SELECT id_rev \
-                        FROM reviews \
-                        WHERE id_req = {id_req[0][0]};")
-        id_rev = cur.fetchall()
-    
-    if len(id_rev) > 0:
-        return 
+        cur.execute("""SELECT MAX(r.request_id)
+                        FROM requests AS r
+                        JOIN users AS u USING(user_id)
+                        WHERE u.tm_hash_id = %s""", (tm_hash_id, ))
+        request_id = cur.fetchall()
+
+    if len(request_id) == 0:
+        return
+
+    # Проверяем пользователь оставлял ли уже отзыв на рекомендацию
+    with db.cursor() as cur:
+        cur.execute("""SELECT review_id
+                        FROM reviews
+                        WHERE request_id = %s""", (request_id[0][0], ))
+        review_id = cur.fetchall()
+
+    if len(review_id) > 0:
+        return
 
     # Добавляем отзыв пользователя
     with db.cursor() as cur:
-        cur.execute("INSERT INTO reviews (id_req, rating) \
-                        VALUES (%s, %s)",
-                        (id_req[0][0], rating))
-    
+        cur.execute("""INSERT INTO reviews (request_id, rating)
+                        VALUES (%s, %s)""", (request_id[0][0], rating))
+
     db.commit()
