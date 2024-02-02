b8d590f9f32944acb308d36a53ffa98855554744
Vlad15lav <vladislav.viryasov@gmail.com>
2024-02-02 15:31:10 +0300

    change cookiecutter template

---
+++ src/config.py
@@ -0,0 +1,52 @@
+import os
+
+CONFIG = {
+    "TOKEN": os.environ.get('TELEGRAM_TOKEN'),
+    "DATABASE": {
+        "host": os.environ.get('DATABASE_HOST'),
+        "user": os.environ.get('DATABASE_USER'),
+        "passwd": os.environ.get('DATABASE_PASSWORD'),
+        "database": os.environ.get('DATABASE_NAME'),
+    },
+    
+    "A/B_STATUS": False,
+    "MODEL_SELECT": "TF-IDF", # Options: "TF-IDF", "BERT"
+    "DATA_ITEMS": "data/food-dataset-ru.csv",
+    "BERT_ITEMS_PATH": "data/bert_items_embed.npy",
+    "TFIDF_ITEMS_PATH": "data/tfidf_items_embed.npz",
+    "TFIDF_FEATURES": "weights/tfidf-model.pkl",
+    "TOKENIZER_PATH": "cointegrated/rubert-tiny2",
+    "BERT_WEIGHT": "weights/bert-food-cls.pth",
+    "BERT_CONFIG": {
+        "architectures": [
+        "BertForPreTraining"
+        ],
+        "attention_probs_dropout_prob": 0.1,
+        "emb_size": 312,
+        "gradient_checkpointing": False,
+        "hidden_act": "gelu",
+        "hidden_dropout_prob": 0.1,
+        "hidden_size": 312,
+        "initializer_range": 0.02,
+        "intermediate_size": 600,
+        "layer_norm_eps": 1e-12,
+        "max_position_embeddings": 2048,
+        "model_type": "bert",
+        "num_attention_heads": 12,
+        "num_hidden_layers": 3,
+        "pad_token_id": 0,
+        "position_embedding_type": "absolute",
+        "torch_dtype": "float32",
+        "transformers_version": "4.30.2",
+        "type_vocab_size": 2,
+        "use_cache": True,
+        "vocab_size": 83828
+    },
+    "TOKENIZER": {
+        "add_special_tokens": True,
+        "max_length": 256,
+        "padding": 'max_length',
+        "truncation": True,
+        "return_tensors": 'pt'
+    }
+}
---
+++ src/utils/loader.py
@@ -0,0 +1,62 @@
+import os
+import gdown
+import pickle
+import torch
+import numpy as np
+import pandas as pd
+import scipy.sparse as sparse
+import wget
+import zipfile
+
+from torch import nn
+from transformers import BertConfig, BertForSequenceClassification, BertTokenizer
+from config import CONFIG
+
+
+def load_files():
+    if not (os.path.exists(CONFIG["DATA_ITEMS"]) \
+        and os.path.exists(CONFIG["TFIDF_ITEMS_PATH"]) \
+        and os.path.exists(CONFIG["TFIDF_FEATURES"])):
+            gdown.download("https://drive.google.com/uc?id=17nDmHE84dT76vsVrq-5RaqrEoJBdFMV7",
+                "recsys_data.zip", quiet=False)
+            
+            with zipfile.ZipFile("recsys_data.zip", 'r') as zip_ref:
+                zip_ref.extractall(".")
+            
+            os.remove("recsys_data.zip")
+            wget.download("https://github.com/Vlad15lav/food-recsys/releases/download/v0.1.0/bert-food-cls.pth",
+                out="weights/bert-food-cls.pth")
+
+def load_data():
+    """
+    Чтение данных набора данных и эмбеддингов
+    """
+    data_items = pd.read_csv(CONFIG["DATA_ITEMS"])
+
+    bert_embed_items = np.load(CONFIG["BERT_ITEMS_PATH"])
+    tfidf_embed_items = sparse.load_npz(CONFIG["TFIDF_ITEMS_PATH"])
+
+    return data_items, bert_embed_items, tfidf_embed_items
+
+def load_model_tfidf():
+    """
+    Загрузка модели TF-IDF
+    """
+    tfidf_model = pickle.load(open(CONFIG["TFIDF_FEATURES"], "rb"))
+
+    return tfidf_model
+
+
+def load_model_bert():
+    """
+    Загрузка модели BERT и Tokenizer
+    """
+    model = BertForSequenceClassification(BertConfig(**CONFIG["BERT_CONFIG"]))
+    model.classifier = nn.Linear(312, 13)
+
+    model.load_state_dict(torch.load(CONFIG["BERT_WEIGHT"], map_location='cpu'))
+    model = model.bert.eval()
+
+    tokenizer = BertTokenizer.from_pretrained(CONFIG["TOKENIZER_PATH"])
+
+    return model, tokenizer
